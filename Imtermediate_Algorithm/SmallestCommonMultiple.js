f3188154213ae6c62707418a931ff7e0da11f66b
Shaowei <zsw22@vip.qq.com>
2017-08-16 12:52:08 +1000

    Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.

---
+++ Imtermediate_Algorithm/SmallestCommonMultiple.js
@@ -0,0 +1,25 @@
+function smallestCommons(arr) {
+    var max = Math.max(...arr);
+    var min = Math.min(...arr);
+    var candidate = max;
+
+    var smallestCommon = function(low, high) {
+        // inner function to use 'high' variable
+        function scm(l, h) {
+            if (h % l === 0) {
+                return h;
+            } else {
+                return scm(l, h + high);
+            }
+        }
+        return scm(low, high);
+    };
+
+    for (var i = min; i <= max; i += 1) {
+        candidate = smallestCommon(i, candidate);
+    }
+
+    return candidate;
+}
+
+smallestCommons([1,5]);
