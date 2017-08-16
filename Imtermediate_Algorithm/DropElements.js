6ffa9406df9f5e35af74c223032217227ae54110
Shaowei <zsw22@vip.qq.com>
2017-08-16 13:22:16 +1000

    Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
    The second argument, func, is a function you'll use to test the first elements of the array to decide if you should drop it or not.
    
    Return the rest of the array, otherwise return an empty array.

---
+++ Imtermediate_Algorithm/DropElements.js
@@ -0,0 +1,10 @@
+
+function dropElements(arr, func) {
+    // Drop them elements.
+    while(!func(arr[0])){
+        arr.shift();
+    }
+    return arr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
